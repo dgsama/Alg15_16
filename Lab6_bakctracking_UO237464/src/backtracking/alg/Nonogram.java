636d3cdd69845da77861002b02b8b969b83f9ea9
dgsama <UO237464@EII-L-S-02-01.aulas.uniovi.es>
2016-04-13 15:59:00 +0200

    parse lab6

--- Lab6_bakctracking_UO237464/src/backtracking/alg/Nonogram.java
+++ Lab6_bakctracking_UO237464/src/backtracking/alg/Nonogram.java
@@ -1,24 +1,49 @@
 package backtracking.alg;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Nonogram {
 
 	public int[][] squares;
+	public ArrayList<ArrayList<Integer>> rowsNumbers;
+	public ArrayList<ArrayList<Integer>> columnsNumbers;
+	private int size;
 
-	private static final int black = 35;
-	private static final int white = 79;
+	private static final char black = 'x';
+	private static final char white = '.';
 
-	public static void main(String[] args) {
-		Nonogram a = new Nonogram(4, 4);
-		a.squares[2][2] = 1;
-		a.squares[0][0] = 1;
-		a.squares[1][1] = 1;
-		a.squares[3][3] = 1;
+	public static void main(String[] args) throws FileNotFoundException {
+		Nonogram case1 = new Nonogram("case1.txt");
 
-		a.show();
+		case1.show();
 	}
 
-	public Nonogram(int a, int b) {
-		squares = new int[a][b];
+	public Nonogram(String file) throws FileNotFoundException {
+		rowsNumbers = new ArrayList<ArrayList<Integer>>();
+		columnsNumbers = new ArrayList<ArrayList<Integer>>();
+
+		ArrayList<ArrayList<Integer>> aux = parseFile(file);
+
+		for (int i = 0; i < aux.size(); i++) {
+			if (i < getSize()) {
+				rowsNumbers.add(aux.get(i));
+			} else {
+				columnsNumbers.add(aux.get(i));
+			}
+		}
+		squares = new int[getSize()][getSize()];
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
 	}
 
 	private void show() {
@@ -32,6 +57,33 @@ public class Nonogram {
 			}
 			System.out.println("");
 		}
+	}
+
+	public ArrayList<ArrayList<Integer>> parseFile(String file)
+			throws FileNotFoundException {
+		FileReader f = new FileReader(file);
+		BufferedReader br;
+
+		ArrayList<ArrayList<Integer>> lines = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> auxI = new ArrayList<Integer>();
+		String[] auxS;
+		try {
+			br = new BufferedReader(f);
+			setSize(Integer.parseInt(br.readLine()));
+
+			while ((auxS = br.readLine().split("")) != null) {
+				for (int j = 0; j < auxS.length; j++) {
+					auxI.add(Integer.parseInt(auxS[j]));
+				}
+				lines.add(auxI);
+			}
+
+			br.close();
+			f.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines;
 	}
 
 }
