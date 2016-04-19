37e1f917b8f3f2e1525b220c2e4bcd20d28724c5
dgsama <dgsama141@gmail.com>
2016-04-19 23:52:58 +0200

    F***

--- Lab6_bakctracking_UO237464/src/backtracking/alg/Nonogram.java
+++ Lab6_bakctracking_UO237464/src/backtracking/alg/Nonogram.java
@@ -9,12 +9,13 @@ import java.util.ArrayList;
 public class Nonogram {
 
 	public static boolean[][] squares;
-	public ArrayList<ArrayList<Integer>> rowsNumbers;
-	public ArrayList<ArrayList<Integer>> columnsNumbers;
+	public static ArrayList<ArrayList<Integer>> rowsNumbers;
+	public static ArrayList<ArrayList<Integer>> columnsNumbers;
 
-	private int size;
+	private static int size;
 	private static final char black = 'x';
 	private static final char white = '.';
+	private static boolean found = false;
 
 	/**
 	 * Constructor for the class Nonogram
@@ -74,8 +75,7 @@ public class Nonogram {
 	 *         problem.
 	 * @throws FileNotFoundException
 	 */
-	public ArrayList<ArrayList<Integer>> parseFile(String file)
-			throws FileNotFoundException {
+	public ArrayList<ArrayList<Integer>> parseFile(String file) throws FileNotFoundException {
 
 		FileReader f = new FileReader(file);
 		BufferedReader br;
@@ -90,8 +90,8 @@ public class Nonogram {
 			String a = "";
 			while ((a = br.readLine()) != null) {
 				auxS = a.split(" ");
+				auxI = new ArrayList<Integer>();
 				for (int j = 0; j < auxS.length; j++) {
-					auxI = new ArrayList<Integer>();
 					auxI.add(Integer.parseInt(auxS[j]));
 				}
 				lines.add(auxI);
@@ -105,12 +105,161 @@ public class Nonogram {
 	}
 
 	public void calculate() {
-		boolean[][] result = squares;
-		recCalculate(result);
-		squares = result;
+		backtracking(0, 0);
 	}
 
-	private void recCalculate(boolean[][] result) {
+	private void backtracking(int x, int y) {
+
+		if (isASolution(x)) {
+			found = true;
+			show();
+		} else {
+			for (int i = 0; i < 2; i++) {
+				if (i == 0) {
+					squares[x][y] = false;
+				} else if (i == 1) {
+					squares[x][y] = true;
+				}
+
+				if (restriccionesF(x) == false && restricionC(y) == false) {
+					// Primer if restriccionesFilaContador
+					// Segundo cambioFila
+					// else
+					if (cambioFila(x, y) && restriccionesFilaContador(x) == true) {// si
+																					// pasa
+																					// asegurarse
+																					// de
+																					// que
+																					// no
+																					// hay
+																					// restriccion
+																					// en
+																					// toda
+																					// la
+																					// fila
+						backtracking(x + 1, 0);
+						squares[x][y] = false;
+					} else if (cambioFila(x, y) && restriccionesFilaContador(x) == false) {
+						squares[x][y] = false;
+					} else {
+						backtracking(x, y + 1);
+						squares[x][y] = false;
+					}
+
+				}
+			}
+		}
+	}
+
+	private boolean restriccionesF(int row) {
+		ArrayList<Integer> row_list = rowsNumbers.get(row);
+		int cont = 0;
+		int contglob = 0;
+		int r = 0;
+		int res = row_list.get(r);
+		for (int i = 0; i < size; i++) {
+			if (res != 0) {
+				if (squares[row][i] == true) {
+					cont = cont + 1;
+					contglob = contglob + 1;
+				} else {
+					if (cont <= res) {
+						cont = 0;
+						r = r + 1;
+						if (r < row_list.size())
+							res = row_list.get(r);
+					} else
+						return true;
+				}
+
+			}
+		}
+		return false;
+	}
+
+	private boolean restricionC(int column) {
+		ArrayList<Integer> col_list = columnsNumbers.get(column);
+		int cont = 0;
+		int r = 0;
+		int res = col_list.get(r);
+		for (int i = 0; i < size; i++) {
+			if (res != 0) {
+				if (squares[i][column] == true) {
+					cont = cont + 1;
+				} else {
+					if (cont <= res) {
+						cont = 0;
+						r = r + 1;
+						if (r < col_list.size())
+							res = col_list.get(r);
+					} else
+						return true;
+				}
+			}
+		}
+		return false;
 
 	}
+
+	private boolean cambioFila(int x, int y) {
+		y = y + 1;
+		if (y == size)
+			return true;
+		return false;
+	}
+
+	private boolean restriccionesFilaContador(int fila) {
+		ArrayList<Integer> r_list = rowsNumbers.get(fila);
+		int contres = 0;
+		int contnono = 0;
+		int res;
+		for (int i = 0; i < r_list.size(); i++) {
+			res = r_list.get(i);
+			if (res != 0)
+				contres += res;
+		}
+		for (int i = 0; i < size; i++) {
+			if (squares[fila][i] == true)
+				contnono += 1;
+		}
+		if (contnono == contres)
+			return true;
+		return false;
+	}
+
+	private boolean isASolution(int x) {
+		if (x == size) {
+			return true;
+		}
+		return false;
+	}
+
+	public void imprimirCondicionesFilas() {
+		ArrayList<ArrayList<Integer>> aux = rowsNumbers;
+
+		System.out.println("Filas");
+		for (int i = 0; i < aux.size(); i++) {
+			ArrayList<Integer> aux1 = aux.get(i);
+			for (int j = 0; j < aux1.size(); j++) {
+				System.out.print(aux1.get(j) + ", ");
+			}
+			System.out.println("");
+		}
+
+	}
+	
+	public void imprimirCondicionescolumnas() {
+		ArrayList<ArrayList<Integer>> aux = columnsNumbers;
+		
+		System.out.println("columnas");
+		for (int i = 0; i < aux.size(); i++) {
+			ArrayList<Integer> aux1 = aux.get(i);
+			for (int j = 0; j < aux1.size(); j++) {
+				System.out.print(aux1.get(j) + ", ");
+			}
+			System.out.println("");
+		}
+
+	}
+
 }
--- Lab6_bakctracking_UO237464/src/backtracking/alg/NonogramTest.java
+++ Lab6_bakctracking_UO237464/src/backtracking/alg/NonogramTest.java
@@ -3,29 +3,27 @@ package backtracking.alg;
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class NonogramTest {
 
 	@Test
-	public void testA() throws FileNotFoundException {
+	public void testA() throws IOException {
 		Nonogram case1 = new Nonogram("case1.txt");
+
 		case1.calculate();
-	
-		boolean[][] expectedResult = { { false, false, true, true, true },
-				{ false, false, true, false, false },
-				{ true, true, true, true, false },
-				{ true, true, false, false, false },
+
+		boolean[][] expectedResult = { { false, false, true, true, true }, { false, false, true, false, false },
+				{ true, true, true, true, false }, { true, true, false, false, false },
 				{ false, true, false, true, true } };
-		
+
 		for (int i = 0; i < expectedResult.length; i++) {
 			for (int j = 0; j < expectedResult[0].length; j++) {
 				assertEquals(expectedResult[i][j], case1.squares[i][j]);
 			}
 		}
-
-		case1.show();
 	}
 
 }
